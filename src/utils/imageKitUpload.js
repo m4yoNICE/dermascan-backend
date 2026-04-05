45c4a1236faa6b81f9869db03294a16d54926922
m4yoNICE <cenizaroswellrey@gmail.com>
2026-04-05 17:35:11 +0800

    adjust: time buffer imagekit

--- src/utils/imageKitUpload.js
+++ src/utils/imageKitUpload.js
@@ -4,20 +4,24 @@ import { ENV } from "../config/env.js";
 const imagekit = new ImageKit({
   privateKey: ENV.IMAGEKIT_PRIVATE_KEY,
 });
+
 export async function uploadToImageKit(buffer, fileName, folder = "/general") {
   const controller = new AbortController();
   const timeout = setTimeout(() => controller.abort(), 10000); // 10s timeout
 
   try {
-    const result = await imagekit.files.upload(
-      {
-        file: buffer,
-        fileName,
-        folder,
-      },
-      { signal: controller.signal },
-    );
-    return result.url;
+  const start = Date.now();
+  console.log(`[ImageKit] Starting upload...`);
+  const result = await imagekit.files.upload(
+    {
+      file: buffer,
+      fileName,
+      folder,
+    },
+    { signal: controller.signal },
+  );
+  console.log(`[ImageKit] Upload done in ${Date.now() - start}ms`);
+  return result.url;
   } finally {
     clearTimeout(timeout);
   }
