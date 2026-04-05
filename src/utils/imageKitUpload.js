59f6fa5f706247904cef1b7c1773c4b867915b24
m4yoNICE <cenizaroswellrey@gmail.com>
2026-04-05 16:29:43 +0800

    fix: changed imagekit endpoint to match with latest sdk requirement

--- src/utils/imageKitUpload.js
+++ src/utils/imageKitUpload.js
@@ -2,12 +2,10 @@ import ImageKit from "@imagekit/nodejs";
 import { ENV } from "../config/env.js";
 
 const imagekit = new ImageKit({
-  publicKey: ENV.IMAGEKIT_PUBLIC_KEY,
   privateKey: ENV.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: ENV.IMAGEKIT_URL_ENDPOINT,
 });
 export async function uploadToImageKit(buffer, fileName, folder = "/general") {
-  const result = await imagekit.upload({
+  const result = await imagekit.files.upload({
     file: buffer,
     fileName,
     folder,
